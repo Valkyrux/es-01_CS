a78a4939cedfcd73178c9dc015c21a251d7d84cc
Admin <Admin@DESKTOP-IN7U4B6>
2022-05-10 17:31:09 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace myApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Prodotto tra due numeri da input:");
+            int v1, v2;
+
+            Console.WriteLine("Inserisci il primo numero");
+            while (!int.TryParse(Console.ReadLine(), out v1))
+            {
+                Console.WriteLine("Numero non valido, reiserire il primo numero");
+            }
+
+            Console.WriteLine("Inserisci il secondo numero");
+            while (!int.TryParse(Console.ReadLine(), out v2))
+            {
+                Console.WriteLine("Numero non valido, reiserire il secondo numero");
+            }
+
+            Console.WriteLine("Il risultato: {0}", multiply(v1, v2));
+
+        }
+
+        static int multiply(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
